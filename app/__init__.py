e80d9324becebe84df19032f8a9566116cafbd6f
Cong yiwei <1056285392@qq.com>
2020-06-17 22:50:39 +0800

    '结构初始化'

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020-06-17 22:46
+# @Author  : CongYiWei
+# @Email   : 1056285392@qq.com
+# @File    : __init__.py.py
+# @Software: PyCharm
+from flask import Flask
+
+app = Flask(__name__)
+
+# 其他初始化东西，数据库
